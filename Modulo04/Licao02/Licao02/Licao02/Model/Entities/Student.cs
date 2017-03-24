1302f7abfc7a2dfb2baabb179444a231b673de0f
Cleiton Ferreira <cleitonferreiraa@hotmail.com>
2017-03-23 22:38:53 -0300

    Módulo04

---
+++ Modulo04/Licao02/Licao02/Licao02/Model/Entities/Student.cs
@@ -0,0 +1,57 @@
+﻿using Licao02.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Licao02.Model.Entities
+{
+    public class Student : ObservableBaseObject, IKeyObject
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChange(); }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChange(); }
+        }
+
+        private string group;
+
+        public string Group
+        {
+            get { return group; }
+            set { group = value; OnPropertyChange(); }
+        }
+
+        private string studentNumber;
+
+        public string StudentNumber
+        {
+            get { return studentNumber; }
+            set { studentNumber = value; OnPropertyChange(); }
+        }
+
+        private double average;
+
+        public double Average
+        {
+            get { return average; }
+            set { average = value; OnPropertyChange(); }
+        }
+
+        public string Key
+        {
+            get;set;
+        }
+    }
+}
